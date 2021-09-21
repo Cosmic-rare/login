5278c1be2a01c70a2f44a65fb2e68d523df36613
Cosmic-rare <ysolakey1116@gmail.com>
2021-09-21 20:58:10 +0900

    TOKENを読み込むようにした。動いた。

--- server.js
+++ server.js
@@ -1,16 +1,31 @@
 var express = require('express');
-const get_user = require('./modules/get_user')
+var request = require('request');
 var app = express();
 const port = 3000;
+require('dotenv').config()
+
+const headers = {
+    "Authorization": "Bot " + process.env.TOKEN
+}
+
+function get_user(userid) {
+    const options = {
+        url: `https://discord.com/api/v9/users/${userid}`,
+        method: "GET",
+        headers: headers,
+    };
+
+    request(options, function (error, response, body) {
+        return body;
+    });
+};
 
 app.set("view engine", "ejs");
 
 app.get('/:userid/:token', function (req, res) {
     var userid = req.params.userid;
-    const user = get_user.get(userid);
-    console.log(user);
-    const name = JSON.parse(user).username;
-    res.render("index", { userid: name });
+    const user = get_user(userid);
+    res.render("index");
 });
 
 app.listen(port, () => {

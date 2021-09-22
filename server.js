f3c41b5fdceb847e1231f621bd213f0bcebf832a
Cosmic-rare <ysolakey1116@gmail.com>
2021-09-22 21:23:13 +0900

    同期処理にした

--- server.js
+++ server.js
@@ -9,22 +9,16 @@ app.set("view engine", "ejs");
 app.get('/:userid/:token', function (req, res) {
     var userid = req.params.userid;
 
-    const headers = {
-        "Authorization": "Bot " + process.env.TOKEN
-    }
-
     const options = {
         url: `https://discord.com/api/v9/users/${userid}`,
         method: "GET",
         headers: { "Authorization": "Bot " + process.env.TOKEN },
     };
-    console.log("1")
     request(options, function (err, response, body) {
-        console.log("2")
         username = JSON.parse(body).username;
+        res.render("index", { user: username });
     })
-    console.log("3")
-    res.render("index", { user: username });
+    
 });
 
 app.listen(port, () => {
